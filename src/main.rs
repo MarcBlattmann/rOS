2947028dfe6ed33bcce5753091545349e9c7fdca
Stardo <marc.blattmann08@gmail.com>
2025-03-16 12:16:09 +0100

    Easy testing added

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 use core::panic::PanicInfo;
 mod vga_buffer;
 
 //Start of the os
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
+    println!("Hello World!");
+
+    #[cfg(test)]
+    test_main();
 
     loop {
         //Loop to infinity
@@ -16,6 +22,40 @@ pub extern "C" fn _start() -> ! {
 //This function is called on panic / error
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
+    println!("{info}");
     loop {}
+}
+
+
+//Testing
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+
+    exit_qemu(QemuExitCode::Success);
+}
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
